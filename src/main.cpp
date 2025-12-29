5328e5f979ce6e6f00daa3197577bd1895fd6d6f
jungwoo0719 <jungwoo07191@naver.com>
2025-12-29 16:47:13 +0900

    RTC_Module

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,56 @@
-#include <Arduino.h>
+#include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <RTClib.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+RTC_DS1307 RTC;
+
+#define SDA_PATH 21
+#define SCL_PATH 22
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+void setup () {
+
+  Serial.begin(115200);
+
+  Wire.begin(SDA_PATH, SCL_PATH);
+
+  RTC.begin();
+
+  RTC.adjust(DateTime(F(__DATE__), F(__TIME__)));
+ 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop(){
+  DateTime now = RTC.now();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.print(now.year(), DEC);  // 년
+
+  Serial.print('/');
+
+  Serial.print(now.month(), DEC);  // 월
+
+  Serial.print('/');
+
+  Serial.print(now.day(), DEC);   // 일
+
+  Serial.print(' ');
+
+  Serial.print(now.hour(), DEC);    // 시간
+
+  Serial.print(':');
+
+  Serial.print(now.minute(), DEC);   // 분
+
+  Serial.print(':');
+
+  Serial.print(now.second(), DEC);   // 초
+
+  Serial.print(' ');
+
+  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);   // 요일
+
+  Serial.println();
+
+  delay(1000);
 }
