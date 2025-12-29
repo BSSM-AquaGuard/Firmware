bf4ec9040c1b95dfdc22fbbfd61c6add4dacc0bc
bitbyte08 <me@bitworkspace.kr>
2025-12-30 08:46:36 +0900

    feat :: LoRa Task 분리

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <SPI.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <freertos/queue.h>
@@ -6,12 +7,30 @@
 #include <Lora.hpp>
 #include "pin.h"
 
+#include "tasks/lora_task.hpp"
+
+QueueHandle_t txQueue;
+LoRaTaskContext loraCtx;
+
 Lora lora(Serial2, LORA_M0_PIN, LORA_M1_PIN, LORA_AUX_PIN);
 
 void setup() {
   Serial.begin(115200);
-  Serial2.begin(9600, SERIAL_8N1, LORA_SERIAL_RX_PIN, LORA_SERIAL_TX_PIN); 
-  lora.setMode(MODE_NORMAL);
+  Serial2.begin(9600, SERIAL_8N1, LORA_SERIAL_RX_PIN, LORA_SERIAL_TX_PIN);
+  
+  txQueue = xQueueCreate(10, sizeof(DataPacket));
+  
+  loraCtx.lora = &lora;
+  loraCtx.txQueue = txQueue;
+  xTaskCreatePinnedToCore(
+    LoRaTxTask,
+    "LoRaTxTask",
+    4096,
+    &loraCtx,
+    1,
+    NULL,
+    1
+  );
 }
 
 void loop() {}
