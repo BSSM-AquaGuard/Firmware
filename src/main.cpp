0b31dd823e93f9203bbf8c97db1fb790ad13a93c
jungwoo0719 <jungwoo07191@naver.com>
2025-12-29 17:56:49 +0900

    SEN0189

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <Wire.h>
-
+#include "ArduinoJson.h"
 #include <RTClib.h>
 
 RTC_DS1307 RTC;
 
 #define SDA_PATH 21
 #define SCL_PATH 22
+#define TURBIDITY 34
 
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
@@ -22,6 +23,14 @@ void setup () {
 }
 
 void loop(){
+
+  int sensorValue = analogRead(TURBIDITY); // read the input on analog pin 0:
+
+  float voltage = sensorValue * (5.0 / 1024.0); // Convert the analog reading (0-1023) to a voltage (0-5V)
+
+  Serial.println(voltage); // print out the value you read:
+
+  delay(500);
   DateTime now = RTC.now();
 
   Serial.print(now.year(), DEC);  // 년
@@ -52,5 +61,5 @@ void loop(){
 
   Serial.println();
 
-  delay(1000);
-}
+  delay(1000); 
+} 
