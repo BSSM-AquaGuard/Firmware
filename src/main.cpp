bab32892aac6d71b6066e0937c0b205197cf9a17
bitbyte08 <me@bitworkspace.kr>
2025-12-30 14:55:08 +0900

    feat :: struct 선언 수정

--- src/main.cpp
+++ src/main.cpp
@@ -18,33 +18,6 @@ SensorTaskContext sensorCtx;
 
 Lora lora(Serial2, LORA_M0_PIN, LORA_M1_PIN, LORA_AUX_PIN);
 
-void scanI2C() {
-  Serial.println("\n[I2C Scanner] Scanning...");
-  
-  int nDevices = 0;
-  for(byte address = 1; address < 127; address++) {
-    Wire.beginTransmission(address);
-    byte error = Wire.endTransmission();
-    
-    if (error == 0) {
-      Serial.print("[I2C Scanner] Device found at 0x");
-      if (address < 16) Serial.print("0");
-      Serial.println(address, HEX);
-      nDevices++;
-    } else if (error == 4) {
-      Serial.printf("[I2C Scanner] Unknown error at 0x%02X\n", address);
-    }
-  }
-  
-  if (nDevices == 0) {
-    Serial.println("[I2C Scanner] No I2C devices found!");
-    Serial.println("[I2C Scanner] Check: VCC, GND, SDA, SCL connections");
-    Serial.println("[I2C Scanner] Check: Pull-up resistors on SDA/SCL");
-  } else {
-    Serial.printf("[I2C Scanner] Found %d device(s)\n", nDevices);
-  }
-}
-
 void setup() {
   Serial.begin(115200);
   delay(1000);
@@ -52,9 +25,6 @@ void setup() {
   // Lolin D32: I2C 클럭 속도를 낮춰서 시도
   Wire.begin();
   Wire.setClock(10000);  // 10kHz로 낮춤 (기본 100kHz)
-  Serial.printf("[I2C] Using default board pins - SDA=%d, SCL=%d at 10kHz\n", SDA, SCL);
-  
-  scanI2C();  // I2C 스캔 먼저 실행
   
   Serial2.begin(9600, SERIAL_8N1, LORA_SERIAL_RX_PIN, LORA_SERIAL_TX_PIN);
   
